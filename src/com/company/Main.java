83b54715607088e8fe5cdc8b4e744bb3720f1e40
DavidGabelgaard <davi493c@edu.easj.dk>
2021-09-02 11:22:17 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        //Write your code here!
+        System.out.println("Hello World!");
+    }
+
+
+
+
+}
