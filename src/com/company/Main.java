afabc92b92f97f7db4773d78ef1d5008ee0c44ba
DavidGabelgaard <davi493c@edu.easj.dk>
2021-09-02 11:51:16 +0200

    Just addet a little text

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         //Write your code here!
         System.out.println("Hello World!");
+        System.out.println("My name is David");
     }
 
 
